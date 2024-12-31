e6c13cc659f9da673dd7e00cf7f758b1ffb635ad
Viktor Nikulaev <viktor_1611@mail.ru>
2024-12-31 16:25:16 +0300

    create model blog, user

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+from users.models import NULLABLE
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Заголовок', help_text='Введите заголовок')
+    content = models.TextField(verbose_name='Содержание', help_text='Введите текст статьи')
+    image = models.ImageField(upload_to='blog/', verbose_name='Изображение', help_text='Выберите изображение',
+                              **NULLABLE)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания', help_text='Дата создания статьи')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата изменения',
+                                      help_text='Дата последнего изменения статьи')
+    author = models.ForeignKey('users.User', verbose_name='Автор', on_delete=models.CASCADE, related_name='blog_posts',
+                               **NULLABLE)
+    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+    views_count = models.PositiveIntegerField(default=0, verbose_name='Количество просмотров')
+    is_subscribed = models.BooleanField(default=False, verbose_name='Подписка')
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
+
+    def __str__(self):
+        return self.title
+
+    class Category(models.Model):
+        name = models.CharField(max_length=50, verbose_name='Название', help_text='Введите название категории')
+
+        class Meta:
+            verbose_name = 'Категория'
+            verbose_name_plural = 'Категории'
+
+        def __str__(self):
+            return self.name
--- users/models.py
+++ users/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class User(AbstractUser):
+    name = models.CharField(max_length=50, verbose_name='Имя', help_text='Введите имя')
+    phone = models.CharField(max_length=20, unique=True, verbose_name='Номер телефона',
+                             help_text='Введите номер телефона')
+    email = models.EmailField(unique=True, verbose_name='Электронная почта', help_text='Введите электронную почту')
+    tg_nick = models.CharField(max_length=50, unique=True, verbose_name='Ник в Telegram',
+                               help_text='Введите ник в Telegram', **NULLABLE)
+    avatar = models.ImageField(upload_to='users/', verbose_name='Аватар', help_text='Выберите изображение', **NULLABLE)
+    is_subscribed = models.BooleanField(default=False, verbose_name='Подписка')
+
+    USERNAME_FIELD = 'phone'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return f'{self.phone} - {self.name}'
